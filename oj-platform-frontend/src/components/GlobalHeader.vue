e09a85133c2fe5e9afafe3c8629af65e90ec448d
holic-x <holic-x@foxmail.com>
2024-04-22 22:46:54 +0800

    feat：frontend 项目通用布局构建
    【1】构建上中下布局
    【2】依次填充各个布局的内容（菜单（Menu组件引入）、内容、底部）
    【3】网站logo、title设置

---
+++ oj-platform-frontend/src/components/GlobalHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="globalHeader">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/oj-logo.png" />
+          <div class="title">Noob OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">Solution</a-menu-item>
+      <a-menu-item key="3">Cloud Service</a-menu-item>
+      <a-menu-item key="4">Cooperation</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<!-- 设置setup vue3写法 -->
+<script setup lang="ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.globalHeader {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 40px;
+  background-color: var(--color-neutral-2);
+}
+
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  margin-left: 16px;
+}
+</style>
---
+++ oj-platform-frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader></GlobalHeader>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <!--        <router-view />-->
+        <h1>页面定义</h1>
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="http://noob.holic-x.com" target="_blank"> 一人の境 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #42b983;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
