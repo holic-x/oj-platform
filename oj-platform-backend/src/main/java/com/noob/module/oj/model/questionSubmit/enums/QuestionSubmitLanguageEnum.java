8f41a57effdb6c04bc42a2cba2d0724696cf63c3
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-04-23 11:43:50 +0800

    refactor：backend 引入QuestionSubmitLanguageEnum
    backend 引入QuestionSubmitLanguageEnum

---
+++ oj-platform-backend/src/main/java/com/noob/module/oj/model/questionSubmit/enums/QuestionSubmitLanguageEnum.java
@@ -0,0 +1,68 @@
+package com.noob.module.oj.model.questionSubmit.enums;
+
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @ClassName Huh-x
+ * @Description TODO
+ * @Author Huh-x
+ * @Date 2024 2024/4/23 11:38
+ */
+public enum QuestionSubmitLanguageEnum {
+
+    JAVA("Java", "Java"),
+
+    CPLUSPLUS("C++", "C++"),
+
+    GOLANG("Golang", "Golang");
+
+
+    private final String text;
+
+    private final String value;
+
+    QuestionSubmitLanguageEnum(String text, String value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    /**
+     * 获取值列表
+     *
+     * @return
+     */
+    public static List<String> getValues() {
+        return Arrays.stream(values()).map(item -> item.value).collect(Collectors.toList());
+    }
+
+    /**
+     * 根据 value 获取枚举
+     *
+     * @param value
+     * @return
+     */
+    public static QuestionSubmitLanguageEnum getEnumByValue(String value) {
+        if (ObjectUtils.isEmpty(value)) {
+            return null;
+        }
+        for (QuestionSubmitLanguageEnum anEnum : QuestionSubmitLanguageEnum.values()) {
+            if (anEnum.value.equals(value)) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
