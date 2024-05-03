76a5674ae2b7371490aa14f81b17bb6ecc6b342d
holic-x <holic-x@foxmail.com>
2024-05-03 13:11:11 +0800

    feat：backend 示例沙箱构建
    backend 示例沙箱构建：
    - 示例沙箱构建，帮助完成业务流程
    - 引入QuestionSubmitStatusEnum、JudgeInfoMessageEnum枚举

--- oj-platform-backend/src/main/java/com/noob/module/judge/codesandbox/impl/ExampleCodeSandbox.java
+++ oj-platform-backend/src/main/java/com/noob/module/judge/codesandbox/impl/ExampleCodeSandbox.java
@@ -4,6 +4,8 @@ import com.noob.module.judge.codesandbox.CodeSandbox;
 import com.noob.module.judge.codesandbox.model.ExecuteCodeRequest;
 import com.noob.module.judge.codesandbox.model.ExecuteCodeResponse;
 import com.noob.module.judge.codesandbox.model.JudgeInfo;
+import com.noob.module.oj.model.questionSubmit.enums.JudgeInfoMessageEnum;
+import com.noob.module.oj.model.questionSubmit.enums.QuestionSubmitStatusEnum;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -18,22 +20,22 @@ import java.util.List;
 public class ExampleCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
-//        List<String> inputList = executeCodeRequest.getInputList();
-//        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
-//        executeCodeResponse.setOutputList(inputList);
-//        executeCodeResponse.setMessage("测试执行成功");
-////        executeCodeResponse.setStatus(QuestionSubmitStatusEnum.SUCCEED.getValue());
-//        JudgeInfo judgeInfo = new JudgeInfo();
-////        judgeInfo.setMessage(JudgeInfoMessageEnum.ACCEPTED.getText());
-//        judgeInfo.setMemory(100L);
-//        judgeInfo.setTime(100L);
-//        executeCodeResponse.setJudgeInfo(judgeInfo);
-//        return executeCodeResponse;
 
         System.out.println("示例代码沙箱");
-        return null;
+        List<String> inputList = executeCodeRequest.getInputList();
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        executeCodeResponse.setOutputList(inputList);
+        executeCodeResponse.setMessage("测试执行成功");
+        executeCodeResponse.setStatus(QuestionSubmitStatusEnum.SUCCEED.getValue());
+        JudgeInfo judgeInfo = new JudgeInfo();
+        judgeInfo.setMessage(JudgeInfoMessageEnum.ACCEPTED.getText());
+        judgeInfo.setMemory(100L);
+        judgeInfo.setTime(100L);
+        executeCodeResponse.setJudgeInfo(judgeInfo);
+        return executeCodeResponse;
+//        System.out.println("示例代码沙箱");
+//        return null;
 
     }
-
 }
 
