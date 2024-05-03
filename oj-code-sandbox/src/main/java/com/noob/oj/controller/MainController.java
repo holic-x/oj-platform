179441cca4ad21cf7de0a575b2294a01badc766f
holic-x <holic-x@foxmail.com>
2024-05-03 18:54:28 +0800

    init：init oj-code-sandbox
    init oj-code-sandbox：
    - 初始化代码沙箱项目
    - 测试代码编译、执行流程

---
+++ oj-code-sandbox/src/main/java/com/noob/oj/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.noob.oj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName MainController
+ * @Description TODO
+ * @Author holic-x
+ * @Date 2024/5/3 17:51
+ */
+@RestController("/")
+public class MainController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok";
+    }
+}
+
+
+
+
---
+++ oj-code-sandbox/src/test/java/com/noob/oj/OjCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noob.oj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
